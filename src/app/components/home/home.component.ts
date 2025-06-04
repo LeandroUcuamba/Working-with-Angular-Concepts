f788c53c34a829e3f1fc585fb27fc25a8b7531bd
Leandro Ucuamba <leandrodossantos.lc5@gmail.com>
2025-06-04 21:33:37 +0100

    add input

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, Input } from '@angular/core';
 import { EnviaFormularioService } from '../../services/envia-formulario.service';
 
 @Component({
@@ -13,6 +13,8 @@ export class HomeComponent {
   idButton = "jdjdjdjd"
   deveMostrarTitulo = false;
   listItems = ["Um","Dois","Três"];
+
+  @Input("name") testeee!: string;
 
   submit(){
     this.enviaFormularioService.enviarInformacaoParaBackend("processando")
