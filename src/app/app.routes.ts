bb0d49aa918d745a01094753b024f72134eb4278
Leandro Ucuamba <leandrodossantos.lc5@gmail.com>
2025-06-05 22:49:08 +0100

    add 'CanActivate'

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,14 @@
 import { Routes } from '@angular/router';
 import { HelloComponent } from './initial/hello/hello.component';
 import { ListaComponent } from './initial/lista/lista.component';
+import { HelloGuardService } from './initial/hello/hello.guard.service';
 
 export const routes: Routes = [
     {
         path: "hello",
         component: HelloComponent,
         children: [
-            { path: "lista", component: ListaComponent }
+            { path: "lista", component: ListaComponent, canActivate: [HelloGuardService] }
         ]
     },
     {
