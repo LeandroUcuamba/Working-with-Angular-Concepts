fa22b6fd295e8350dd6f6389dbb31a40f1800c70
Leandro Ucuamba <leandrodossantos.lc5@gmail.com>
2025-06-07 12:21:12 +0100

    Add forms, but without any validation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { HelloComponent } from './initial/hello/hello.component';
 import { ListaComponent } from './initial/lista/lista.component';
 import { HelloGuardService } from './initial/hello/hello.guard.service';
+import { FormsComponent } from './initial/forms/forms.component';
 
 export const routes: Routes = [
     {
@@ -15,5 +16,9 @@ export const routes: Routes = [
     {
         path: "lista",
         component: ListaComponent
-    }
+    },
+    {
+        path: "forms",
+        component: FormsComponent
+    },
 ];
