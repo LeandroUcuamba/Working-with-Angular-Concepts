871dfdab7ddcd76d78036bddc3bcbfb43d1f8753
Leandro Ucuamba <leandrodossantos.lc5@gmail.com>
2025-06-07 13:10:20 +0100

    Add service - get API

--- src/app/initial/hello/hello.component.ts
+++ src/app/initial/hello/hello.component.ts
@@ -1,5 +1,6 @@
-import { Component, EventEmitter, Input, Output, signal } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output, signal } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { HelloService } from './hello.service';
 
 @Component({
   selector: 'app-hello',
@@ -7,7 +8,22 @@ import { RouterModule } from '@angular/router';
   templateUrl: './hello.component.html',
   styleUrl: './hello.component.css'
 })
-export class HelloComponent {
+export class HelloComponent implements OnInit{
+
+  constructor(private service: HelloService){
+
+  }
+  ngOnInit(): void {
+    this.service.getPoke()
+        .subscribe(
+          res => {
+            this.pokelist = res.result;
+            console.log(res);
+          }
+        )
+  }
+
+  pokelist: any;
 
   Acao(){
     console.log("Ação Executada!");
