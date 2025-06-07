fc37ba33a9d5fd0b70c903347cdc15e268e91e86
Leandro Ucuamba <leandrodossantos.lc5@gmail.com>
2025-06-07 12:44:33 +0100

    Add validação no form

--- src/app/initial/forms/forms.component.ts
+++ src/app/initial/forms/forms.component.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-forms',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './forms.component.html',
   styleUrl: './forms.component.css'
 })
 export class FormsComponent {
+    frm: FormGroup;
 
+    constructor(private fb: FormBuilder){
+      this.frm = this.fb.group(
+        {
+           'nome':['', [Validators.required]],
+           'endereco':['', [Validators.maxLength(300)]],
+           'email':['', [Validators.required, Validators.email]],
+        }
+      )
+    }
+
+    salvar(){
+      console.log(this.frm.value);
+    }
 }
